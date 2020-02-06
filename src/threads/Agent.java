c9474c9568814388384d972be5e137c9ece0c0f8
Micka108 <mickael.lecocq@outlook.com>
2020-02-06 16:27:35 -0500

    Window

---
+++ src/threads/Agent.java
@@ -0,0 +1,21 @@
+package threads;
+
+public class Agent extends Thread {
+
+    private Object sensors;
+
+	public Agent(){
+        super();
+        
+        // Sensor
+
+
+    }
+
+    @Override
+    public void run() {
+        while(true){
+        }
+    }
+
+}
