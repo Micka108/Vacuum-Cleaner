386e49c6f8a2ec295f53a18f8eb64ea9d882fda3
Micka108 <mickael.lecocq@outlook.com>
2020-02-08 11:52:34 -0500

    ADD

--- src/threads/Agent.java
+++ src/threads/Agent.java
@@ -1,24 +1,52 @@
 package threads;
 
+
 import java.util.ArrayList;
+import java.util.List;
 
 import search.Node;
 
 public class Agent extends Thread {
 
     private AgentSensors sensors;
-    public ArrayList<Node> perents;
 
 	public Agent(int x, int y) throws InterruptedException{
         super();
         //Sensors
         AgentSensors sensors = new AgentSensors(x, y, new Environnement());
     }
+	
+	private void BFS(Node start, ArrayList<Node> openSet, ArrayList<Node> closelSet) {
+		
+		openSet.add(start);
+		
+		while (openSet.isEmpty() == false) {
+			Node current = openSet.get(0);
+			openSet.remove(0);
+			if (goal(current.gridState) == true)
+		}
+	}
+	
+	
+	private boolean goal(int[][] gridState) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (gridState[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+		return true;
+		
+	}
 
     @Override
     public void run() {
         while(true){
+    		ArrayList<Node> openSet = new ArrayList<Node>();
+    		ArrayList<Node> closelSet = new ArrayList<Node>();
         }
     }
 
+    
 }
