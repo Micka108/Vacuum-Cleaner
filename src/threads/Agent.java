e3bf908d254788599c7a999201374b3c2e26e852
VIGER-Valentin <vigervalentin@gmail.com>
2020-02-08 11:12:43 -0500

    ClosestElement function

--- src/threads/Agent.java
+++ src/threads/Agent.java
@@ -3,9 +3,11 @@ package threads;
 public class Agent extends Thread {
 
     private AgentSensors sensors;
+    private boolean informed;
 
 	public Agent(int x, int y) throws InterruptedException{
         super();
+        this.informed = false;
         //Sensors
         AgentSensors sensors = new AgentSensors(x, y, new Environnement());
     }
